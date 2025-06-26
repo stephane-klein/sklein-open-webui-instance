2a6a2e2190f19e2f7e40b5cec529d3213d048cdc
stephane-klein <contact@stephane-klein.info>
2025-06-26 17:41:29 +0200

    Add dummy variable Valves in hello_world_function

--- functions/hello_world_function.py
+++ functions/hello_world_function.py
@@ -2,11 +2,12 @@ from pydantic import BaseModel, Field
 
 class Pipe:
     class Valves(BaseModel):
-        pass
+        FOO: str = Field(default="bar")
 
     def __init__(self):
         self.valves = self.Valves()
 
     def pipe(self, body: dict):
+        print("Valves FOO", self.valves.FOO)
         print("body", body)
-        return "Hello, World!"
+        return f"Hello, World! {self.valves.FOO}"
